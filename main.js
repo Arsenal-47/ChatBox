ad857ef434a8c0f3436efe841274cd8e6ceb8bf0
Naman Gautam <73691403+Arsenal-47@users.noreply.github.com>
2021-11-16 23:32:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function sendmsg(){
+  var  msg=document.getElementById('msg')
+      msg =msg.value
+  var  sendMsg=document.getElementsByClassName('sent')
+
+    createMsg(msg,"8 PP")
+    
+    
+}
+function createMsg(msgshow,time){
+    var  sendMsg=document.getElementsByClassName('sent')
+
+    continer = document.createElement('div')
+        sentMsg = document.createElement('div')
+        sentMsg.className="sentmsgArea"
+            msg= document.createElement("p")
+            msg.innerHTML = msgshow
+            sentMsg.append(msg)
+        dateDiv= document.createElement('div')
+        dateDiv.className="datediv"
+            date = document.createElement('p')
+            date.innerHTML=time
+        dateDiv.append(date)
+    continer.append(sentMsg)
+    continer.append(dateDiv)
+sendMsg[0].append( continer)
+
+
+}
